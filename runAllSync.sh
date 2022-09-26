23b3ee6daea7194a4d2a70554c523728fb343e39
Chris Marino <christopher.marino@salesforce.com>
2022-09-26 11:05:11 -0700

    Add sync to test runs

--- runAllSync.sh
+++ runAllSync.sh
@@ -6,12 +6,10 @@ c=1
 remain=50
 rm *.log
 while [ $c -le $jobs ]; do 
-    #sleep 1
-    sfdx force:apex:execute -f test_apex/runQnaSync.apex -u test-mzczhjcguvwk@example.com  >> outSyncQna.log &
-    sleep 1
-    #sleep 1
     sfdx force:apex:execute -f test_apex/runSentimentSync.apex -u test-mzczhjcguvwk@example.com >> outSyncClass.log &
-    sleep 1
+    sleep .5
+    #sfdx force:apex:execute -f test_apex/runQnaSync.apex -u test-mzczhjcguvwk@example.com  >> outSyncQna.log &
+    #sleep .5
     echo 'Another 2 Sync runs that queue function invocations.' $c 'out of' $jobs 'batches started.' 
     c=$(($c+1))
     
