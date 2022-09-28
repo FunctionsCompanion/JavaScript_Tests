48d11dcaa8f069161d31792b376423439ed07651
Chris Marino <christopher.marino@salesforce.com>
2022-09-28 05:11:02 -0700

    New package and logger

--- runAllAsync.sh
+++ runAllAsync.sh
@@ -6,8 +6,8 @@ c=1
 remain=50
 rm *.log
 while [ $c -le $jobs ]; do 
-    sfdx force:apex:execute -f test_apex/runSentimentAsync.apex -u test-mzczhjcguvwk@example.com >> outAsyncClass.log &
-    sleep .5
+    sfdx force:apex:execute --loglevel=DEBUG -f test_apex/runSentimentAsync.apex -u test-mzczhjcguvwk@example.com >> outAsyncClass.log &
+    sleep 2
     #sfdx force:apex:execute -f test_apex/runQnaAsync.apex -u test-mzczhjcguvwk@example.com >> outAsyncQna.log &
     #sleep .5
     echo 'Another Aync runs that invokes 5 async functions.' $c 'out of' $jobs 'batches started.' 
