dae8bd355812e658b74168fcdccc51a266ccb56d
Chris Marino <christopher.marino@salesforce.com>
2022-10-06 06:59:19 -0700

    New org with package v1.32

--- runAllAsync.sh
+++ runAllAsync.sh
@@ -6,9 +6,9 @@ c=1
 remain=50
 rm *.log
 while [ $c -le $jobs ]; do 
-    sfdx force:apex:execute --loglevel=DEBUG -f test_apex/runSentimentAsync.apex -u test-mzczhjcguvwk@example.com >> outAsyncClass.log &
+    sfdx force:apex:execute --loglevel=DEBUG -f test_apex/runSentimentAsync.apex -u test-y0dfmejs8rzd@example.com >> outAsyncClass.log &
     sleep .5
-    #sfdx force:apex:execute -f test_apex/runQnaAsync.apex -u test-mzczhjcguvwk@example.com >> outAsyncQna.log &
+    #sfdx force:apex:execute -f test_apex/runQnaAsync.apex -u test-y0dfmejs8rzd@example.com >> outAsyncQna.log &
     #sleep .5
     echo 'Another Aync runs that invokes 5 async functions.' $c 'out of' $jobs 'batches started.' 
     c=$(($c+1))
--- runAllSync.sh
+++ runAllSync.sh
@@ -6,9 +6,9 @@ c=1
 remain=50
 rm *.log
 while [ $c -le $jobs ]; do 
-    sfdx force:apex:execute -f test_apex/runSentimentSync.apex -u test-mzczhjcguvwk@example.com >> outSyncClass.log &
+    sfdx force:apex:execute -f test_apex/runSentimentSync.apex -u test-y0dfmejs8rzd@example.com >> outSyncClass.log &
     sleep .5
-    #sfdx force:apex:execute -f test_apex/runQnaSync.apex -u test-mzczhjcguvwk@example.com  >> outSyncQna.log &
+    #sfdx force:apex:execute -f test_apex/runQnaSync.apex -u test-y0dfmejs8rzd@example.com  >> outSyncQna.log &
     #sleep .5
     echo 'Another 2 Sync runs that queue function invocations.' $c 'out of' $jobs 'batches started.' 
     c=$(($c+1))
