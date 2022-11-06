0ef1040f8f0f790bd51c365d355f26e61b64e90b
Chris Marino <christopher.marino@salesforce.com>
2022-11-05 17:39:06 -0700

    Changed template for Cost tab

--- install.sh
+++ install.sh
@@ -17,9 +17,9 @@ fi
 envsubst < ./data/FC_Settings__cs.tmpl > ./data/FC_Settings__cs.json
 
 # Install the Functions Companion Package in the org
-echo 'Installing Functions Companion v1.35'
-echo 'sfdx force:package:install -r --wait 10 --package 04t8c000001EfHQ -u' ${username}
-sfdx force:package:install -r --wait 10 --package 04t8c000001EfHQ -u "${username}"
+echo 'Installing Functions Companion v1.36'
+echo 'sfdx force:package:install -r --wait 10 --package 04t8c000001EfHV -u' ${username}
+sfdx force:package:install -r --wait 10 --package 04t8c000001EfHV -u "${username}"
 echo ''
 echo 'Configuring the package and installing a dummy API key: XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX.'
 echo 'Be sure to update with a valide API key after you create your Connected App.'
