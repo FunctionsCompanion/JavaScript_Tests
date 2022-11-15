bfd5f0b66edf1500ca3e1924260a754c7431de6a
Chris Marino <christopher.marino@salesforce.com>
2022-11-15 12:37:02 -0800

    New package v1.38

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-#!/bin/bash  
-# Ask users for prefix
+#!/bin/bash 
+# Ask users for prefix 
 echo 'Please provide the username for the target org you wish to install Functions Companion.'
 echo -n 'username:'
 read username
@@ -17,9 +17,9 @@ fi
 envsubst < ./data/FC_Settings__cs.tmpl > ./data/FC_Settings__cs.json
 
 # Install the Functions Companion Package in the org
-echo 'Installing Functions Companion v1.36'
-echo 'sfdx force:package:install -r --wait 10 --package 04t8c000001EfHV -u' ${username}
-sfdx force:package:install -r --wait 10 --package 04t8c000001EfHV -u "${username}"
+echo 'Installing Functions Companion v1.38'
+echo 'sfdx force:package:install -r --wait 10 --package 04t8c000001IscY -u' ${username}
+sfdx force:package:install -r --wait 10 --package 04t8c000001IscY -u "${username}"
 echo ''
 echo 'Configuring the package and installing a dummy API key: XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX.'
 echo 'Be sure to update with a valide API key after you create your Connected App.'
