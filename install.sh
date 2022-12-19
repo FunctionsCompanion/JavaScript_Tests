492ff8befc2711dda69888fcc66ff2d0afcff19a
Chris Marino <christopher.marino@salesforce.com>
2022-12-19 15:22:56 -0800

    New Package with Org Limits DashBoards

--- install.sh
+++ install.sh
@@ -17,9 +17,9 @@ fi
 envsubst < ./data/FC_Settings__cs.tmpl > ./data/FC_Settings__cs.json
 
 # Install the Functions Companion Package in the org
-echo 'Installing Functions Companion v1.38'
-echo 'sfdx force:package:install -r --wait 10 --package 04t8c000001IscY -u' ${username}
-sfdx force:package:install -r --wait 10 --package 04t8c000001IscY -u "${username}"
+echo 'Installing Functions Companion v1.46'
+echo 'sfdx force:package:install -r --wait 10 --package 04t8c000000lD1s -u' ${username}
+sfdx force:package:install -r --wait 10 --package 04t8c000000lD1s -u "${username}"
 echo ''
 echo 'Configuring the package and installing a dummy API key: XXXXXXX-XXXXXXX-XXXXXXX-XXXXXXX.'
 echo 'Be sure to update with a valide API key after you create your Connected App.'
@@ -30,4 +30,4 @@ echo 'sfdx force:data:tree:import -p ./data/FC_Settings__c-plan.json -u' ${usern
 sfdx force:data:tree:import -p ./data/FC_Settings__c-plan.json -u "${username}"
 
 echo 'sfdx force:data:tree:import -p ./data/fcConfig__c-plan.json -u' ${username}
-sfdx force:data:tree:import -p ./data/fcConfig__c-plan.json -u "${username}"
+sfdx force:data:tree:import -p ./data/fcConfig__c-plan.json -u "${username}" 
